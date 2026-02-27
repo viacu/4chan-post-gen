16da55acc4ad79205b9433ef6e0f0dd818beaf45
viacu <ttttgh@via.ci>
2026-02-27 01:53:39 +0100

    think component was missing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+
 
 export const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full', component: App },
   { path: '**', redirectTo: '' }
 ];
